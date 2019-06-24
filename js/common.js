6c275791a958e04923aa60db2c91054ddba2339f
Дмитрий Филиппов <Amoralis.19@gmail.com>
2019-06-24 21:08:04 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$('.toggle').click(function(){
+    $('.toggle-wrap').toggleClass('toggle-wrap-open');
+  });
+	$('.toggle').click(function(){
+    $('.nav').toggleClass('nav-open');
+  });
+
+
+
+});
