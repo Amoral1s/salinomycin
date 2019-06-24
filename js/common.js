b1690960dfacd05e79fabbe7bbfca9d843dab799
Дмитрий Филиппов <Amoralis.19@gmail.com>
2019-06-24 21:23:24 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -7,6 +7,12 @@ $(document).ready(function(){
     $('.nav').toggleClass('nav-open');
   });
 
+  jQuery("a.scrollto").click(function () {
+    elementClick = jQuery(this).attr("href")
+    destination = jQuery(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+    return false;
+  });
 
 
 });
